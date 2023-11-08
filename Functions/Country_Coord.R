37fe553089fa0af4e575b2df948899609eea86d8
Schrodinger73 <63200376+Schrodinger73@users.noreply.github.com>
2023-11-08 22:18:50 +0530

    Update Country_Coord.R

--- Functions/Country_Coord.R
+++ Functions/Country_Coord.R
@@ -21,28 +21,4 @@ for (i in 1:length(r$teams)) {
   }
 }
 coord <- r
-save(coord, file = "Cricket_Coord.RData")
-#data <- left_join(r, team_data, by = "teams")
-#save(data, file = "World_Coord.Rdata")
-#data <- data %>% filter(!is.na(win_loss))
-#data
-#ggplot(data, aes(x = long, y = lat, group = group)) + geom_polygon(aes(fill = win_loss), color = "black")
-teams <- unique(table$Loser)[c(1:6, 8:19, 24:28)]
-W_L <- NULL
-t1 <- 1980
-t2 <- 2021
-t <- table %>% filter((year(`Match Date`) >= t1) & (year(`Match Date`) <= t2))
-for (i in 1:length(Teams)) {
-  win <- sum(t$Winner == Teams[i])
-  loss <- sum(t$Loser == Teams[i])
-  if (loss == 0) {
-    W_L <- append(W_L, NA)
-  }
-  else {
-    W_L <- append(W_L, win/loss)
-  }
-}
-WL <- data.frame(teams, W_L)
-WL
-data <- left_join(coord, WL, by = "teams")
-ggplot(data, aes(x = long, y = lat, group = group)) + geom_polygon(aes(fill = W_L), color = "black")
+save(coord, file = "../Data Sets/Cricket_Coord.RData")
