d7b85884ebd05c9ba37ed1304855bc03e8c873db
Schrodinger73 <63200376+Schrodinger73@users.noreply.github.com>
2023-11-13 13:15:48 +0530

    Update Teams_Data.R

--- Functions/Teams_Data.R
+++ Functions/Teams_Data.R
@@ -20,7 +20,7 @@ for (i in 1971:2023) {
 
 colnames(table) <- c("Team 1", "Team 2", "Winner", "Margin", "Ground", "Match Date", "Scorecard", "Link", "Winner_Score", "Loser_Score")
 
-table <- table %>% filter(Winner != "no result") %>% filter(Winner != "tied")
+table <- table %>% filter(Winner != "no result") %>% filter(Winner != "tied") %>% filter(Margin != "-")
 links <- table$Link
 
 
